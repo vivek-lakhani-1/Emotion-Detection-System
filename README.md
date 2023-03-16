78d3a0c41e688056c15bdddcabbc4468e04171f1
Vivek Lakhani <vlakhani1010@gmail.com>
2023-03-16 21:54:11 +0530

    Data

---
+++ README.md
@@ -0,0 +1,43 @@
+# real-time-face-detection [![Netlify Status](https://api.netlify.com/api/v1/badges/a2e6f8d7-94e7-43f7-b18f-9d51f684c43e/deploy-status)](https://app.netlify.com/sites/lgope-real-time-face-detection/deploys)
+Real Time Face Detection With Face Expression. 😊🙂
+
+## Demonstration :
+### Face Dectect
+![ezgif com-crop](https://user-images.githubusercontent.com/58518192/76557336-93d89100-64c5-11ea-8328-d68f696e87cc.gif)
+
+### Face Detection With Expression
+![ezgif com-crop (1)](https://user-images.githubusercontent.com/58518192/76557341-976c1800-64c5-11ea-9b5c-21cab90810dd.gif)
+
+## Key Features
+Some of the standout features of the project are
+
+ 1. Detect Face
+ 2. Detect Face With Expression
+ 
+## Technologies
+1. ejs
+2. js dom 
+3. [face-api.js](https://github.com/justadudewhohacks/face-api.js)
+
+## Known Bugs
+
+Feel free to email me at lakshman.gope2@gmail.com if you run into any issues or have questions, ideas or concerns. Please enjoy
+and feel free to share your opinion, constructive criticism, or comments about my work. Thank you! 🙂
+
+## Future Updates
+
+- Implement age detection
+- Implement face detect from Images
+
+And More ! There's always room for improvement!
+
+## Instructions
+To run this project :
+1. Clone this repo
+2. Change your directory : `cd real-time-face-detection`
+3. double click index.html
+6. check your browser for view.
+
+## Deployed Version
+
+Feel free to visit 👉 https://lgope-real-time-face-detection.netlify.com/
---
+++ script.js
@@ -0,0 +1,37 @@
+const video = document.getElementById('video');
+const waitInfo = document.getElementById('loading-tag');
+waitInfo.innerHTML = 'Please wait ...';
+
+Promise.all([
+  faceapi.nets.tinyFaceDetector.loadFromUri('./models'),
+  faceapi.nets.faceLandmark68Net.loadFromUri('./models'),
+  faceapi.nets.faceRecognitionNet.loadFromUri('./models'),
+  faceapi.nets.faceExpressionNet.loadFromUri('./models')
+]).then(startVideo);
+
+function startVideo() {
+  navigator.getUserMedia(
+    { video: {} },
+    stream => (video.srcObject = stream),
+    err => console.error(err)
+  );
+  waitInfo.innerHTML = '';
+}
+
+video.addEventListener('play', () => {
+  const canvas = faceapi.createCanvasFromMedia(video);
+  document.body.append(canvas);
+  const displaySize = { width: video.width, height: video.height };
+  faceapi.matchDimensions(canvas, displaySize);
+  setInterval(async () => {
+    const detections = await faceapi
+      .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+      .withFaceLandmarks()
+      .withFaceExpressions();
+    const resizedDetections = faceapi.resizeResults(detections, displaySize);
+    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+    faceapi.draw.drawDetections(canvas, resizedDetections);
+    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections); // face landmarks detection (eye, eyebrow, nose, cheek, lips)
+    faceapi.draw.drawFaceExpressions(canvas, resizedDetections); // face expressions detection
+  }, 100);
+});
